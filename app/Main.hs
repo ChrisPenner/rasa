34b7abe1f79d1ca972f1cb7cbb6977326524f6ea
Chris Penner <christopher.penner@gmail.com>
2016-11-09 16:09:13 -0600

    No more brick

--- app/Main.hs
+++ app/Main.hs
@@ -6,38 +6,30 @@ import VtyAdapter (convertEvent)
 import State
 import View (render)
 
-import Brick.Widgets.Core
-import Brick.Widgets.Border
-import qualified Brick.Types as BT
-import qualified Brick.Main as M
-
 import Control.Lens
 import qualified Data.Text.IO as TIO
 import Data.Default (def)
 
-appEvent :: St -> BT.BrickEvent () e -> BT.EventM () (BT.Next St)
-appEvent st evt = toBrick . handleEvent . toRasa evt $ st
+import Graphics.Vty as V
 
-drawUi :: St -> [BT.Widget ()]
-drawUi st = [txt (render st)]
+appEvent :: St -> V.Event -> Continue
+appEvent st evt = handleEvent . toRasa evt $ st
 
-toRasa :: BT.BrickEvent () e -> St -> Continue
-toRasa (BT.VtyEvent e) st = Continue (toDirective (st^.mode) (convertEvent e)) st
-toRasa _ st = Continue Noop st
-
-toBrick :: Continue -> BT.EventM () (BT.Next St)
-toBrick (Continue e st) = case e of
-                                Exit -> M.halt st
-                                _ -> M.continue st
-
-app :: M.App St e ()
-app =
-    M.App { M.appDraw = drawUi
-          , M.appChooseCursor = M.showFirstCursor
-          , M.appHandleEvent = appEvent
-          , M.appStartEvent = return
-          , M.appAttrMap = const def
-          }
+toRasa :: V.Event -> St -> Continue
+toRasa e st = Continue (toDirective (st^.mode) (convertEvent e)) st
 
 main :: IO ()
-main = TIO.putStr . view text =<< M.defaultMain app def
+main = do
+    cfg <- V.standardIOConfig
+    vty <- V.mkVty cfg
+    eventLoop vty def
+
+eventLoop :: V.Vty -> St -> IO ()
+eventLoop vty st = do
+    let pic = V.picForImage $ text' V.defAttr (render st)
+    update vty pic
+    e <- V.nextEvent vty
+    let (Continue d nextState) = appEvent st e
+    case d of
+      Exit -> shutdown vty
+      _ -> eventLoop vty nextState
