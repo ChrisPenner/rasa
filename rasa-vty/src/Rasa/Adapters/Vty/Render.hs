ee768b95e3c342aa6bbf4145c65d5db9589c8710
Chris Penner <christopher.penner@gmail.com>
2016-12-07 01:49:18 -0600

    Refactor attr rendering code... still a long way to go.

--- rasa-vty/src/Rasa/Adapters/Vty/Render.hs
+++ rasa-vty/src/Rasa/Adapters/Vty/Render.hs
@@ -9,9 +9,9 @@ import Rasa.View
 import Rasa.Adapters.Vty.State
 import Control.Monad.IO.Class
 
+import Data.List
 import qualified Graphics.Vty as V
 import qualified Data.Text as T
-import Data.Monoid ((<>))
 import Control.Lens
 import Control.Arrow (first)
 
@@ -25,35 +25,11 @@ instance Renderable (Buffer Offset) V.Image where
     render (width, _) = do
         txt <- textWrap width . view text
         curs <- view cursor
-        coord <- view $ asCoord.cursor
-        let styled = applyAttrs [(curs, inverse), (curs + 1, V.defAttr)] txt
-            txtLines = foldMap (V.<->) styled V.emptyImage
-            cursorLine = V.text' V.defAttr (T.pack $ show curs <> " | " <> show coord)
-
-        return $ cursorLine V.<-> txtLines
-
+        return $ applyAttrs [(curs, inverse), (curs + 1, V.defAttr)] txt
             where inverse = V.currentAttr `V.withStyle` V.reverseVideo
 
-applyAttrs :: [(Offset, V.Attr)] -> T.Text -> [V.Image]
+applyAttrs :: [(Offset, V.Attr)] -> T.Text -> V.Image
 applyAttrs attrs t = applyAttrs' attrs (T.lines t)
-
-applyAttrs' :: [(Offset, V.Attr)] -> [T.Text] -> [V.Image]
-applyAttrs' _ [] = []
-applyAttrs' [] lines' = fmap (V.text' V.currentAttr) lines'
-applyAttrs' allAttrs@((offset, attr):attrs) (l:lines')
-  | T.length l < offset = plainText l : applyAttrs' (decr (T.length l + 1) allAttrs) lines'
-  | otherwise = let prefix = plainText (T.take offset l) V.<|> V.text' attr ""
-                    suffix = if null rest
-                                then V.emptyImage
-                                else head rest
-                    rest = applyAttrs' (decr offset attrs) (T.drop offset l:lines')
-                 in (prefix V.<|> suffix) : drop 1 rest
-
-decr :: Int ->  [(Offset, V.Attr)] -> [(Offset, V.Attr)]
-decr n = fmap $ first (subtract n)
-
-plainText :: T.Text -> V.Image
-plainText = V.text' V.currentAttr
 
 getSize :: Alteration (Int, Int)
 getSize = do
@@ -67,3 +43,36 @@ render' = do
     let pic = V.picForImage $ render sz editor
     v <- getVty
     liftIO $ V.update v pic
+
+
+type AttrList = [(Offset, V.Attr)]
+decr :: Int -> AttrList -> AttrList
+decr n = fmap $ first (subtract n)
+
+plainText :: T.Text -> V.Image
+plainText = V.text' V.currentAttr
+
+applyAttrs' :: AttrList -> [T.Text] -> V.Image
+applyAttrs' attrs lines' = vertCat $ unfoldr attrLines (attrs, lines')
+  where
+    vertCat = foldr (V.<->) V.emptyImage
+    attrLines :: (AttrList, [T.Text]) -> Maybe (V.Image, (AttrList, [T.Text]))
+    attrLines (_, []) = Nothing
+    attrLines (as, l:ls) = let (img, restAs) = attrLine as l
+                     in Just (img, (decr 1 restAs, ls))
+
+-- Should be able to clean this up and provide better guarantees if I do a scan
+-- over attrs and get each successive mappend of them, then do T.splitAt for
+-- each offset, then apply the attr for each section at the beggining of each
+-- of T.lines within each group. Ugly I know.
+attrLine :: AttrList -> T.Text -> (V.Image, AttrList)
+attrLine [] txt = (plainText txt, [])
+attrLine ((0, attr):attrs) txt = first (V.text' attr "" V.<|>) $ attrLine attrs txt
+attrLine attrs "" = (V.emptyImage, attrs)
+attrLine allAttrs@((offset, _):_) txt
+  -- If the offset is larger, we can add the whole line, then decrement the attr offsets
+  | offset > T.length txt = (plainText txt, decr (T.length txt) allAttrs)
+  -- The offset occurs within the line, apply it in the middle
+  | otherwise = first (prefix V.<|>) suffix
+  where prefix = plainText (T.take offset txt)
+        suffix = attrLine (decr offset allAttrs) (T.drop offset txt)
